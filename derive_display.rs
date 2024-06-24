3a98b95b0f252aae2635472c6a486ff7443e54b7
orgrinrt <ort@hiisi.digital>
2024-06-24 18:21:29 +0300

    make the derive support more explicit by actually matching the ItemImpl's trait_ ident, and panic where one is missing. update readme and bump deps. release::patch

--- derive_display.rs
+++ derive_display.rs
@@ -1,10 +1,9 @@
 use proc_macro::TokenStream;
 
+use proc_macro2::{Ident, TokenStream as TokenStream2};
 use quote::{quote, ToTokens};
-use syn::{parse_str, Generics};
-// TODO: use the one in proc_macro2 crate
 use syn::ItemImpl;
-use syn::__private::TokenStream2;
+use syn::{parse_str, Generics, Path, Type};
 
 #[proc_macro_attribute]
 pub fn derive_display(_attr: TokenStream, item: TokenStream) -> TokenStream {
@@ -17,6 +16,28 @@ pub fn derive_display(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // let (_, tr, _) = parsed.trait_.expect("Expected there to be a trait in an ItemImpl");
     let impl_generics: Generics = parsed.generics;
     let ty = *parsed.self_ty;
+    let tr_path: Path = parsed
+        .trait_
+        .expect("Expected a valid trait implementation")
+        .1;
+    let tr: Ident = tr_path
+        .segments
+        .last()
+        .expect("Expected a valid path for the implemented trait")
+        .ident
+        .clone();
+    let q = match tr.to_string().as_str() {
+        "ToTokens" => derive_from_to_tokens(impl_generics, ty),
+        _ => {
+            panic!("This implementation is not yet supported by `derive_display`")
+        },
+    };
+
+    q.to_tokens(&mut result);
+    result.into()
+}
+
+fn derive_from_to_tokens(impl_generics: Generics, ty: Type) -> TokenStream2 {
     let inner_s: TokenStream2 = parse_str("#s").expect("Expected a valid TokenStream");
     let q = quote! {
         impl #impl_generics Display for #ty {
@@ -27,6 +48,5 @@ pub fn derive_display(_attr: TokenStream, item: TokenStream) -> TokenStream {
             }
         }
     };
-    q.to_tokens(&mut result);
-    result.into()
+    q
 }
